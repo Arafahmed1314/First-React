3572166b2e3abd3c1fdd139b7d5a4e081afd7c32
Naimul Islam <nayemhasan1314@gmail.com>
2024-04-28 21:39:34 +0600

    complete first module

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const name=['araf','ahmed','natok']
+
+const product=[
+  {name:'Photoshop', price:'90taka'},
+  {name:'Illustrator', price:'90taka'},
+  {name:'PDF Reader', price:'90taka'}
+]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+<ul>
+  {
+    product.map(name=><li>{name.name}{name.price}</li>)
+  }
+  <li>{name[0]}</li>
+  <li>{name[1]}</li>
+  <li>{name[2]}</li>
+</ul>
+
+{
+product.map(product=><Product p={product}></Product>)
+}
+<Counter></Counter>
+
+<User></User>
+{
+  name.map(name=><Person name={name}></Person>)
+}
+        {/* <Person name={name[0]} job="student"></Person>
+        <Person name={name[1]} job="cricker"></Person>
+        <Person name={name[2]} job="footboller"></Person> */}
       </header>
     </div>
   );
+}
+
+function User(){
+  const [users,setUsers]=useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => setUsers(json))
+  })
+  return (
+    <div>
+
+     <h3>total User : {users.length}</h3>
+      <ul>
+        {
+          users.map(user=><li>{user.email}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+function  Counter(){
+  const [count,setCount]=useState(10);
+  const handleIncrease=()=>{
+setCount(count+1)
+  }
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={()=>setCount(count+1)}>Increase</button>
+    </div>
+  )
+}
+
+function Product(props){
+  const st={
+    border:'2px solid green',
+    padding:'20px ',
+    width:'300px',
+    height:'300px'
+  }
+
+  const{name,price}=props.p;
+  return(
+    <div style={st}>
+<h1>{name}</h1>
+<h3>{price}</h3>
+<button>Done</button>
+    </div>
+
+  )
+}
+function Person(props){
+  const st={
+    border:'2px solid red' ,
+    margin:'10px',
+    padding:'20px'
+  }
+  return (
+    <div style={st}>
+      <h1>My name: {props.name}</h1>
+    <h3>Job:{props.job}</h3>
+    </div>
+
+  )
 }
 
 export default App;
